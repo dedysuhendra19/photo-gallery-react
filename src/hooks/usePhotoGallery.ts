12b748674549a56fadf7c675d1f2915bc3d7ead6
dedysuhendra19 <rogereded@gmail.com>
2025-12-19 21:47:44 +0700

    saving photo into Filesystem

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -1,4 +1,5 @@
-import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Directory, Filesystem } from '@capacitor/filesystem';
 import { useState } from 'react';
 
 export function usePhotoGallery() {
@@ -10,23 +11,63 @@ export function usePhotoGallery() {
             resultType: CameraResultType.Uri,
             source: CameraSource.Camera,
             quality: 100
-        });
+        }) as Photo;
+
+        
+        const fileName = Date.now() + '.jpeg';
 
         // add newly caputered photo to the beginning of the array of photos
-        const fileName = Date.now() + '.jpeg';
-        const savedImageFile: UserPhoto[] = [
-            {
-                filepath: fileName,
-                webiewPath: capturedPhoto.webPath
-            },
-            ...photos
-        ];
-        
-        // update photos with the new photo
-        setPhotos(savedImageFile);
+        // const savedImageFile: UserPhoto[] = [
+        //     {
+        //         filepath: fileName,
+        //         webiewPath: capturedPhoto.webPath
+        //     },
+        //     ...photos
+        // ];
+
+        // update savedImageFile
+        const savedImageFile = await savePicture(capturedPhoto, fileName);
+
+        // update state with new photo
+        const newPhotos: UserPhoto[] = [savedImageFile, ...photos];
+        setPhotos(newPhotos);
     };
 
-    return { addNewToGallery, photos };
+    const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
+        // Fetch the photo, read as a blob, then convert to base64 format
+        const response = await fetch(photo.webPath!);
+        const blob = await response.blob();
+        const base64Data = (await convertBlobToBase64(blob)) as string;
+
+        const savedFile = await Filesystem.writeFile({
+            path: fileName,
+            data: base64Data,
+            directory: Directory.Data
+        })
+
+        // Use webPath to display the new image instead of base64 since it's
+        // already loaded into memory
+        return {
+            filepath: fileName,
+            webiewPath: photo.webPath
+        };
+    };
+
+    const convertBlobToBase64 = (blob: Blob) => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onerror = reject;
+            reader.onload = () => {
+                resolve(reader.result);
+            }
+            reader.readAsDataURL(blob);
+        })
+    }
+
+    return { 
+        addNewToGallery, 
+        photos 
+    };
 }
 
 export interface UserPhoto {
